b3ac8793f9b839b797eb7edc821a4d877aecde64
Kiyoshi Fukuda <kiyoshi.fukuda@gmail.com>
2020-04-30 08:43:15 +0900

    data import works. minimal varsion.

--- mysnippets.py
+++ mysnippets.py
@@ -1,6 +1,7 @@
 import argparse
 import base64
 import datetime
+import csv
 
 from google.cloud import spanner
 from google.cloud.spanner_v1 import param_types
@@ -11,18 +12,20 @@ def create_database(instance_id, database_id):
     instance = spanner_client.instance(instance_id)
 
     database = instance.database(database_id, ddl_statements=[
-        """CREATE TABLE Singers (
-            SingerId     INT64 NOT NULL,
-            FirstName    STRING(1024),
-            LastName     STRING(1024),
-            SingerInfo   BYTES(MAX)
-        ) PRIMARY KEY (SingerId)""",
-        """CREATE TABLE Albums (
-            SingerId     INT64 NOT NULL,
-            AlbumId      INT64 NOT NULL,
-            AlbumTitle   STRING(MAX)
-        ) PRIMARY KEY (SingerId, AlbumId),
-        INTERLEAVE IN PARENT Singers ON DELETE CASCADE"""
+        """CREATE TABLE Airports (
+            ident   STRING(1024),
+            type    STRING(1024),
+            name    STRING(1024),
+            elevation_ft    INT64,
+            continent   STRING(1024),
+            iso_country STRING(1024),
+            iso_region  STRING(1024),
+            municipality    STRING(1024),
+            gps_code    STRING(1024),
+            iata_code   STRING(1024),
+            local_code  STRING(1024),
+            coordinates STRING(1024)
+        ) PRIMARY KEY (ident)"""
     ])
 
     operation = database.create()
@@ -36,6 +39,39 @@ def create_database(instance_id, database_id):
 
 def insert_data(instance_id, database_id):
     print('insert_data called')
+    spanner_client = spanner.Client()
+    instance = spanner_client.instance(instance_id)
+    database = instance.database(database_id)
+    
+    collist = ['ident', 'type', 'name', 'elevation_ft', 'continent', 'iso_country', 'iso_region', 'municipality','gps_code','iata_code','local_code','coordinates']
+    typelist = ['string','string', 'string', 'integer','string','string','string','string','string','string','string','string']
+    alist = []
+    path = 'data/airport-codes.csv'
+    with open(path) as f:
+        reader = csv.reader(f)
+        for row in reader:
+            for x in range(0, len(collist)):
+                if typelist[x] == 'integer':
+                    if row[x] != '':
+                        row[x] = int(row[x])
+                    else:
+                        row[x] = None
+            alist.append(row)
+    #print(alist)
+
+    rowpos = 0
+    batchsize = 10
+    while rowpos < len(alist):
+        with database.batch() as batch:
+            batch.insert(
+                table = 'Airports',
+                columns=collist,
+                values=alist[rowpos:rowpos+batchsize]
+            )  
+            rowpos = rowpos + batchsize
+            print('batch done') 
+    
+    print('Done')
 
 def delete_data(instance_id, database_id):
     print('delete_data called')
